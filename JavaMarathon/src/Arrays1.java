5582214b455b6b03907c1e3d629c4dbb345e89a7
Victor <victor.hugo.mrts@gmail.com>
2023-08-05 20:46:58 -0300

    cleaning

--- JavaMarathon/src/Arrays1.java
+++ JavaMarathon/src/Arrays1.java
@@ -1,9 +1,14 @@
 public class Arrays1 {
     public static void main(String[] args) {
-        int[] idades = new int[3];
-        idades[0] = 20;
-        idades[1] = 15;
-        idades[2] = 30;
-        System.out.println(idades[2]);
+        int[] idades = {1,2,3};
+        for (int i = 0; i < 3; i++) {
+            System.out.println(idades[i]);
+        }
+
+        String[] array2 = {"Primeira Posição", "Segunda Posição", "Terceira Posição"};
+        for (int i = 0; i < 3; i++) {
+            System.out.println(array2[i]);
+        }
+
     }
 }
