489f24b278ebff0eda96080057e4ea3da9eca1e3
Victor <victor.hugo.mrts@gmail.com>
2023-08-06 11:57:12 -0300

    Stopwatch exercise

---
+++ Stopwatch/src/Stopwatch.java
@@ -0,0 +1,102 @@
+import java.awt.event.*;
+import java.awt.*;
+import javax.swing.*;
+
+public class Stopwatch implements ActionListener {
+    JFrame frame = new JFrame();
+    JButton startButton = new JButton("Start");
+    JButton resetButton = new JButton("Reset");
+    JLabel timeLabel = new JLabel();
+    int elapsedTime = 0;
+    int seconds = 0;
+    int minutes = 0;
+    int hours = 0;
+    boolean started = false;
+    String seconds_string = String.format("%02d", seconds);
+    String minutes_string = String.format("%02d", minutes);
+    String hours_string = String.format("%02d", hours);
+
+    Timer timer = new Timer(1000, new ActionListener() {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            elapsedTime += 1000;
+            hours = (elapsedTime / 3600000);
+            minutes = (elapsedTime / 60000) % 60;
+            seconds = (elapsedTime / 1000) % 60;
+            seconds_string = String.format("%02d", seconds);
+            minutes_string = String.format("%02d", minutes);
+            hours_string = String.format("%02d", hours);
+            timeLabel.setText(hours_string + ":" + minutes_string + ":" + seconds_string);
+
+        }
+    });
+
+    Stopwatch() {
+        timeLabel.setText(hours_string + ":" + minutes_string + ":" + seconds_string);
+        timeLabel.setBounds(10, 10, 200, 100);
+        timeLabel.setBorder(BorderFactory.createBevelBorder(1));
+        timeLabel.setFont(new Font("Verdana", Font.PLAIN, 35));
+        timeLabel.setOpaque(true);
+        timeLabel.setHorizontalAlignment(JTextField.CENTER);
+
+        startButton.setBounds(10, 110, 100, 50);
+        startButton.setFocusable(false);
+        timeLabel.setFont(new Font("Verdana", Font.PLAIN, 25));
+        startButton.addActionListener(this);
+
+        resetButton.setBounds(110, 110, 100, 50);
+        resetButton.setFocusable(false);
+        timeLabel.setFont(new Font("Verdana", Font.PLAIN, 25));
+        resetButton.addActionListener(this);
+
+        frame.add(timeLabel);
+        frame.add(startButton);
+        frame.add(resetButton);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(240, 210);
+        frame.setLayout(null);
+
+        frame.setVisible(true);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == startButton) {
+            if (started == false) {
+                started = true;
+                startButton.setText("Stop");
+                Start();
+            } else {
+                started = false;
+                startButton.setText("Start");
+                Stop();
+            }
+        }
+        if (e.getSource() == resetButton) {
+            started = false;
+            startButton.setText("Start");
+            Reset();
+        }
+    }
+
+    void Start() {
+        timer.start();
+    }
+
+    void Stop() {
+        timer.stop();
+    }
+
+    void Reset() {
+        timer.stop();
+        elapsedTime = 0;
+        seconds = 0;
+        minutes = 0;
+        hours = 0;
+        seconds_string = String.format("%02d", seconds);
+        minutes_string = String.format("%02d", minutes);
+        hours_string = String.format("%02d", hours);
+        timeLabel.setText(hours_string + ":" + minutes_string + ":" + seconds_string);
+    }
+
+}
